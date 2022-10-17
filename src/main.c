aee1fc60f4ea3961041763810642b922994e3296
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-10-18 00:22:28 +0300

    Make sprite system

--- src/main.c
+++ src/main.c
@@ -1,35 +1,203 @@
+#include "stdio.h"
 #include "raylib.h"
 
-#define SCREEN_WIDTH (800)
-#define SCREEN_HEIGHT (450)
+#define OK  0
+#define ERR 1
 
-#define WINDOW_TITLE "Window title"
+#define WINDOW_TITLE_MAX_SIZE 80
 
-int main(void)
-{
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
-    SetTargetFPS(60);
 
-    Texture2D texture = LoadTexture(ASSETS_PATH"test.png"); // Check README.md for how this works
+typedef struct ProjectConfiguration {
+  char    windowTitle[WINDOW_TITLE_MAX_SIZE];
+  Vector2 windowSize;
+  int     targetFPS;
 
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
+  Color   clearColor;
+} ProjectConfiguration;
 
-        ClearBackground(RAYWHITE);
 
-        const int texture_x = SCREEN_WIDTH / 2 - texture.width / 2;
-        const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
-        DrawTexture(texture, texture_x, texture_y, WHITE);
+typedef struct Sprite {
+  Vector2   position;
+  Texture2D texture;
+  int layerId;
+} Sprite;
 
-        const char* text = "OMG! IT WORKS!";
-        const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
-        DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
 
-        EndDrawing();
-    }
+typedef struct SpritePool {
+  Sprite* sprites;
+  int count;
+  int capacity;
+} SpritePool;
 
-    CloseWindow();
 
-    return 0;
+const SpritePool InitSpritePool(int capacity) {
+  const SpritePool spritePool = {
+    .sprites  = MemAlloc(capacity * sizeof(Sprite)),
+    .count    = 0,
+    .capacity = capacity,
+  };
+  return spritePool;
+}
+
+
+const Vector2 GetWindowCenter(ProjectConfiguration config) {
+  const Vector2 window_center = { config.windowSize.x / 2, config.windowSize.y / 2 };
+  return window_center;
+}
+
+
+char* GetAssetDirPath() {
+  return ASSETS_FOLDER_PATH;
+}
+
+char* GetAssetFullPath(char* assetFullPath, const char* assetPath) {
+  // TODO(gr3yknigh1): Make join function
+  sprintf(assetFullPath, "%s%s", GetAssetDirPath(), assetPath);
+  return assetFullPath;
+}
+
+
+Texture2D LoadTextureAsset(char* assetPath) {
+  char textureFullPath[FILENAME_MAX];
+  Texture2D texture = LoadTexture(
+    GetAssetFullPath(textureFullPath, assetPath)
+    );
+  return texture;
+}
+
+
+const Sprite CreateSpriteT(Vector2 position, Texture texture, int layerId) {
+  const Sprite sprite = {
+    .position = {
+      .x = position.x,
+      .y = position.y,
+    },
+    .texture = texture,
+  };
+  return sprite;
+}
+
+
+const Sprite CreateSprite(Vector2 position, char* textureAssetPath, int layerId) {
+  return CreateSpriteT(
+    position,
+    LoadTextureAsset(textureAssetPath),
+    layerId
+    );
+}
+
+
+const int AddSprite2Pool(SpritePool* spritePool, Sprite sprite) {
+  if (spritePool->count >= spritePool->capacity) {
+    return -1;
+  }
+  spritePool->sprites[spritePool->count] = sprite;
+  spritePool->count++;
+  return spritePool->count;
+}
+
+
+const int AddSprite2PoolC(SpritePool* spritePool, Vector2 position, Texture2D texture, int layerId) {
+  return AddSprite2Pool(
+    spritePool,
+    CreateSpriteT(position, texture, layerId)
+  );
+}
+
+
+const int AddSprite2PoolCA(SpritePool* spritePool, Vector2 position, char* textureAssetPath, int layerId) {
+  return AddSprite2Pool(
+    spritePool,
+    CreateSprite(position, textureAssetPath, layerId)
+  );
+}
+
+
+const Sprite GetSpriteFromPool(SpritePool* spritePool, int index) {
+  return spritePool->sprites[index];
+}
+
+
+void DrawSprite(Sprite sprite) {
+  DrawTextureV(sprite.texture, sprite.position, WHITE);
+}
+
+
+void DrawSpritePool(SpritePool* spritePool) {
+  for (int i = 0; i < spritePool->count; i++) {
+    DrawSprite(spritePool->sprites[i]);
+  }
+}
+
+
+Vector2 SubtractVector2(Vector2 vector1, Vector2 vector2) {
+  Vector2 newVector = {
+    .x = vector1.x - vector2.x,
+    .y = vector1.y - vector2.y
+  };
+  return newVector;
+}
+
+
+const Vector2 Vector2Half(Vector2 vector) {
+  const Vector2 vectorHalf = { vector.x / 2, vector.y / 2 };
+  return vectorHalf;
+}
+
+
+const Vector2 Texture2DSize(Texture2D texture) {
+  const Vector2 size = { texture.width, texture.height };
+  return size;
+}
+
+
+Vector2 CenterTexture2D(Vector2 windowSize, Texture2D texture) {
+  Vector2 centered = SubtractVector2(
+    Vector2Half(windowSize),
+    Vector2Half(Texture2DSize(texture))
+  );
+  return centered;
+}
+
+
+int main() {
+
+  const ProjectConfiguration config = {
+    .windowTitle = "C Raylib Platformer",
+    .windowSize  = {
+      .x = 800,
+      .y = 600,
+    },
+    .targetFPS   = 60,
+    .clearColor  = WHITE,
+  };
+
+  const Vector2 windowSize   = config.windowSize;
+  const Vector2 windowCenter = GetWindowCenter(config);
+
+  InitWindow(
+    windowSize.x,
+    windowSize.y,
+    config.windowTitle
+    );
+  SetTargetFPS(config.targetFPS);
+
+  SpritePool spritePool = InitSpritePool(2);
+
+  {
+    const Texture texture = LoadTextureAsset("test.png");
+    const Vector2 position = CenterTexture2D(windowSize, texture);
+    AddSprite2PoolC(&spritePool, position, texture, 0);
+  }
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+      ClearBackground(config.clearColor);
+      DrawSpritePool(&spritePool);
+    EndDrawing();
+  }
+
+  CloseWindow();
+
+  return OK;
 }
